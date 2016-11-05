4e5b63da3fb1fa7fd3a9af66d3bd597564cc91c9
James Lewis <jamesl@thoughtworks.com>
2016-11-05 14:28:28 +1100

    add test endpoint to get data from firebase DB

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var Firebase = require('firebase');
 
-
 var config = {
   apiKey: "AIzaSyCufFUbb5zzaMscOaza0oJDmcV-9ZTdHjY",
   authDomain: "strangers-in-space.firebaseapp.com",
@@ -10,18 +9,20 @@ var config = {
   messagingSenderId: "748579681540"
 };
 Firebase.initializeApp(config);
+Firebase.auth().signInWithEmailAndPassword('jamesl@thoughtworks.com', 'welcome1');
 
 var app = express();
 
 app.get('/test', function (req, res) {
   var db = Firebase.database();
-  var ref = db.ref("test");
+  var ref = db.ref("/test");
   var blah = 2;
   ref.once("value", function(snapshot) {
     blah = snapshot.val();
+    res.send(JSON.stringify({ a: blah }));
+  }, function(error) {
+    console.log(error);
   });
-
-  res.send(blah);
 });
 
 
